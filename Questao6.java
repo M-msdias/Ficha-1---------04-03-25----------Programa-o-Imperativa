2c1f50d4e3f5d16b83edaff5fb336af56304764d
M-msdias <ddiasmarcela@gmail.com>
2025-03-10 16:16:43 -0300

    Create Questao6.java

---
+++ Questao6.java
@@ -0,0 +1,15 @@
+public class Questao6 {
+    public static void main(String[] args) {
+
+        System.out.println("Asteriscos no formato de triângulo isósceles:");
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+    
+
